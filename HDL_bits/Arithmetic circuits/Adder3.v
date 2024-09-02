d49d7b4e197e45f8bf13b28d17f22d0eba724650
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-02 07:31:16 -0700

    Update Adder3.v

--- HDL_bits/Arithmetic circuits/Adder3.v
+++ HDL_bits/Arithmetic circuits/Adder3.v
@@ -1,1 +1,15 @@
-
+module top_module( 
+    input [2:0] a, b,
+    input cin,
+    output [2:0] sum,
+    output [2:0] cout );
+    fa in0(a[0],b[0],cin,sum[0],cout[0]);
+    fa in1(a[1],b[1],cout[0],sum[1],cout[1]);
+    fa in2(a[2],b[2],cout[1],sum[2],cout[2]);
+endmodule
+module fa(a,b,cin,sum,cout);
+    input a,b,cin;
+    output sum ,cout;
+    assign sum=a^b^cin;
+    assign cout=(a&b)+(b&cin)+(a&cin);
+endmodule
