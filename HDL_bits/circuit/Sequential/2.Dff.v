57908f4a9af44c56ae2a8c93d413a2ae9794b8dc
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2024-09-03 03:22:14 -0700

    Create 2.Dff.v

---
+++ HDL_bits/circuit/Sequential/2.Dff.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input d, 
+    input r,   // synchronous reset
+    output q);
+    always @(posedge clk)begin
+        if(r==1)
+            q<=1'b0;
+        else
+            q<=d;
+    end
+endmodule
